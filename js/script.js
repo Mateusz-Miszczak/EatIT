354b0e1936657d89c9fa0c089142f1c88d3ae3c9
Mateusz Miszczak <mthw.miszczak@gmail.com>
2022-10-27 23:25:35 +0200

    Making mobile navigation bar work, correcting misspeled class name, linking script.js to html file

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////////////////
+// Make mobile navigation bar work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////////////////
